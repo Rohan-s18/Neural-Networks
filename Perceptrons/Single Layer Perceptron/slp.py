9220cd2f9d5f63a790d8baed66fab2248e2952ee
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-28 18:52:17 -0800

    Added slp file

---
+++ Perceptrons/Single Layer Perceptron/slp.py
@@ -0,0 +1,32 @@
+"""
+Author: Rohan Singh
+12/28/2022
+This Python Module contains source code for a single layer perceptron 
+This code will be generalized to produce biniary output for an n-dimension numpy input vector
+"""
+
+#Imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class SLP:
+
+    def __init__(self, n, arr):
+        self.n = n
+        self.arr = arr
+    
+
+
+
+
+
+def main():
+
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
+
