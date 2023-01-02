271b970b70f96aa76f10cd1d16d632683716e5ae
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-02 10:05:01 -0800

    Added template code for Multi-layer preceptron

---
+++ Perceptrons/Multi Layer Perceptron/mlp.py
@@ -0,0 +1,49 @@
+"""
+Author: Rohan Singh
+1/02/2022
+This Python Module contains source code for a multi-layer perceptron 
+This code will be generalized to produce an n-dimension output vector for an m-dimension numpy input vector
+"""
+
+
+#Imports
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#%%
+"""
+This cell contains source code for the Multi-Layer Perceptron class
+"""
+
+class MLP:
+
+    #Constructor
+    def __init__(self, n, m, trainset, target):
+        self.n = n
+        self.m = m
+        self.trainset = trainset
+        self.target = target
+
+    
+
+
+
+
+
+#%%
+
+"""
+This cell contains the main method demonstration
+"""
+
+def main():
+
+    print("Hello World!\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
